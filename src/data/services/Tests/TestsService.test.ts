1ccf097103b89aab7fa7b78b0145fae7fb8b39dc
hanashiro <felipeaah@gmail.com>
2022-11-06 10:28:41 -0300

    feat: :sparkles: Tests Service
    It tests the values like a test framework

---
+++ src/data/services/Tests/TestsService.test.ts
@@ -0,0 +1,21 @@
+import { TestsService } from './TestsService';
+
+describe('TestsService', () => {
+    // beforeAll(() => {
+    //     jest.useFakeTimers();
+    //     jest.spyOn(global, 'setTimeout');
+    // });
+    // afterAll(() => {
+    //     jest.useRealTimers();
+    //     jest.clearAllTimers();
+    // });
+    it('should test TestsService', () => {
+        // expect(true).toBe(true);
+        // const spy = jest
+        //     .spyOn(TestsService, TestsService.test.name as keyof typeof TestsService);
+        //     .mockReturnValue(123);
+        // expect(spy).toBeCalledTimes(1);
+        // const mockedTestsService = jest.mocked(TestsService);
+        // mockedTestsService.test.mockReturnValue(456);
+    });
+});
---
+++ src/data/services/Tests/TestsService.ts
@@ -0,0 +1,306 @@
+interface DescriptionInterface {
+    name: string;
+    it: ItInterface[];
+}
+
+interface ItInterface {
+    name: string;
+    expects: ExpectInterface[];
+}
+
+interface ExpectInterface {
+    name: string;
+    status: boolean;
+}
+
+export class TestsService {
+    private beforeEachs: Function[] = [];
+    private afterEachs: Function[] = [];
+    private afterAlls: Function[] = [];
+    private beforeAlls: Function[] = [];
+    private totalTests = 0;
+    private passedTests = 0;
+    private failedTests = 0;
+    private stats = [] as DescriptionInterface[];
+    private currentDescription = {
+        name: '',
+        it: [],
+    } as DescriptionInterface;
+    private currentIt = {
+        name: '',
+        expects: [],
+    } as ItInterface;
+
+    beforeEach(fn: Function) {
+        this.beforeEachs.push(fn);
+    }
+
+    afterEach(fn: Function) {
+        this.afterEachs.push(fn);
+    }
+
+    beforeAll(fn: Function) {
+        this.beforeAlls.push(fn);
+    }
+
+    afterAll(fn: Function) {
+        this.afterAlls.push(fn);
+    }
+
+    expect(value: unknown) {
+        return {
+            toBe: (expected: unknown) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    value === expected,
+                    `Expected: {{value}} / Received: {{expected}}`
+                );
+            },
+            toBeTruthy: () => {
+                this.createMatcher(
+                    value,
+                    undefined,
+                    !!value,
+                    `Expected: {{value}} to be truthy`
+                );
+            },
+            toBeFalsy: () => {
+                this.createMatcher(
+                    value,
+                    undefined,
+                    !value,
+                    `Expected: {{value}} to be falsy`
+                );
+            },
+            toBeGreaterThan: (expected: number) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as number) > expected,
+                    `Expected: {{value}} to be greater than {{expected}}`
+                );
+            },
+            toBeGreaterThanOrEqual: (expected: number) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as number) >= expected,
+                    `Expected: {{value}} to be greater than or equal to {{expected}}`
+                );
+            },
+            toBeLessThan: (expected: number) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as number) < expected,
+                    `Expected: {{value}} to be less than {{expected}}`
+                );
+            },
+            toBeLessThanOrEqual: (expected: number) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as number) <= expected,
+                    `Expected: {{value}} to be less than or equal to {{expected}}`
+                );
+            },
+            toBeDefined: () => {
+                this.createMatcher(
+                    value,
+                    undefined,
+                    value !== undefined,
+                    `Expected: {{value}} to be defined`
+                );
+            },
+            toBeUndefined: () => {
+                this.createMatcher(
+                    value,
+                    undefined,
+                    value === undefined,
+                    `Expected: {{value}} to be undefined`
+                );
+            },
+            toBeNull: () => {
+                this.createMatcher(
+                    value,
+                    null,
+                    value === null,
+                    `Expected: {{value}} to be null`
+                );
+            },
+            toBeNaN: () => {
+                this.createMatcher(
+                    value,
+                    NaN,
+                    Number.isNaN(value),
+                    `Expected: {{value}} to be NaN`
+                );
+            },
+            toBeInstanceOf: (expected: any) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    value instanceof expected,
+                    `Expected: {{value}} to be instance of {{expected}}`
+                );
+            },
+            toContain: (expected: unknown) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as unknown[]).includes(expected),
+                    `Expected: {{value}} to contain {{expected}}`
+                );
+            },
+            toHaveLength: (expected: number) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as unknown[]).length === expected,
+                    `Expected: {{value}} to have length {{expected}}`
+                );
+            },
+            toHaveProperty: (expected: string) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    (value as object).hasOwnProperty(expected),
+                    `Expected: {{value}} to have property {{expected}}`
+                );
+            },
+            toMatch: (expected: RegExp) => {
+                this.createMatcher(
+                    value,
+                    expected,
+                    expected.test(value as string),
+                    `Expected: {{value}} to match {{expected}}`
+                );
+            },
+            not: {
+                toBe: (expected: unknown) => {
+                    this.createMatcher(
+                        value,
+                        expected,
+                        value !== expected,
+                        `Expected: not {{value}} / Received: {{expected}}`
+                    );
+                },
+                toBeInstanceOf: (expected: any) => {
+                    this.createMatcher(
+                        value,
+                        expected,
+                        !(value instanceof expected),
+                        `Expected: {{value}} not to be instance of {{expected}}`
+                    );
+                },
+                toContain: (expected: unknown) => {
+                    this.createMatcher(
+                        value,
+                        expected,
+                        !(value as unknown[]).includes(expected),
+                        `Expected: {{value}} not to contain {{expected}}`
+                    );
+                },
+                toHaveLength: (expected: number) => {
+                    this.createMatcher(
+                        value,
+                        expected,
+                        (value as unknown[]).length !== expected,
+                        `Expected: {{value}} not to have length {{expected}}`
+                    );
+                },
+                toHaveProperty: (expected: string) => {
+                    this.createMatcher(
+                        value,
+                        expected,
+                        !(value as object).hasOwnProperty(expected),
+                        `Expected: {{value}} not to have property {{expected}}`
+                    );
+                },
+                toMatch: (expected: RegExp) => {
+                    this.createMatcher(
+                        value,
+                        expected,
+                        !expected.test(value as string),
+                        `Expected: {{value}} not to match {{expected}}`
+                    );
+                },
+            },
+        };
+    }
+
+    private createMatcher(
+        value: unknown,
+        expected: unknown,
+        status: boolean,
+        message: string = 'Expected: {{value}} / Received: {{expected}}'
+    ) {
+        const expectation = {
+            name: message
+                .replace('{{value}}', `${value}`)
+                .replace('{{expected}}', `${expected}`),
+            status: status,
+        } as ExpectInterface;
+
+        this.currentIt.expects.push(expectation);
+        this.updateTestsResultsNumber(status);
+    }
+
+    private updateTestsResultsNumber(status: boolean) {
+        if (status) {
+            this.passedTests++;
+        } else {
+            this.failedTests++;
+        }
+    }
+
+    it(description: string, fn: Function) {
+        this.totalTests++;
+        this.beforeEachs.forEach((fn) => fn.apply(this));
+
+        this.currentIt = {
+            name: description,
+            expects: [],
+        };
+
+        fn.apply(this);
+
+        this.afterEachs.forEach((fn) => fn.apply(this));
+
+        this.currentDescription.it.push(this.currentIt);
+    }
+
+    describe(description: string, fn: Function) {
+        this.beforeAlls.forEach((fn) => fn.apply(this));
+
+        this.currentDescription = {
+            name: description,
+            it: [],
+        };
+
+        fn.apply(this);
+
+        this.afterAlls.forEach((fn) => fn.apply(this));
+
+        this.stats.push(this.currentDescription);
+    }
+
+    showResults() {
+        console.log(`Tests: ${this.passedTests} passed, ${this.totalTests} total
+`);
+        this.stats.forEach((description) => {
+            console.log(description.name);
+            description.it.forEach((it) => {
+                console.log(`    ${it.name}`);
+                it.expects.forEach((expectation) => {
+                    console.log(
+                        `        ${expectation.status ? '√' : 'X'} ${
+                            expectation.name
+                        }`
+                    );
+                });
+            });
+        });
+    }
+}
